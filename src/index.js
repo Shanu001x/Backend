cda1b25051ddb4473c9fdd0073ceb5e3418cd4f1
Shanu001x <sraw419@gmail.com>
2024-07-07 13:06:37 +0530

    Make Server and utils

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+// require('dotenv').config()
+
+// 
+import dotenv from "dotenv"
+import connectDb from "./db/index.js"
+import { app } from "./app.js"
+
+dotenv.config({
+    path: '.env'
+})
+
+connectDb()
+.then(() => {
+    app.on("Error", (error) => {
+        console.log("Error: ", error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at Port: ${process.env.PORT}`)
+    })  
+})
+    
+    
+.catch((err)=> {
+    console.log('Database connection failed', err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* import express from "express"
 import mongoose from "mongoose";
 import { DB_NAME } from "./constant";
