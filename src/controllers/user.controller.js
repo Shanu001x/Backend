747a488d3944f7aeb62622eb7a5676cff83eb930
Shanu001x <sraw419@gmail.com>
2024-07-20 10:28:07 +0530

    Writing logic for Register Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,27 @@
+// Steps follow krna hi algorithm hai - sahi step
+
 import { asyncHandler } from "../utiles/asyncHandler.js";
+import {ApiError} from "../utiles/ApiError.js"
 
 const registerUser = asyncHandler(async(req, res) => {
-     res.status(200).json({
-        message: "ok"
-    })
+   // get user details from frontend
+   // validation not empty
+   // Check if user already exist: email, username
+   // check for images, check for avatar
+   // upload them to cloudinary - avatar
+   // create user object - create entry in DB
+   // remove password and refresh token field from response
+   // return res
+
+
+   const {fullname, username, email, password} = req.body
+   console.log("email: ", email);
+
+   if (
+      [fullname, email, password, username].some((field) => field?.trim() === "")
+   ) {
+      throw new ApiError(400, "fullName is required") 
+   }
 })
 
 export {registerUser};
