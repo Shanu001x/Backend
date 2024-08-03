944f7abc69b4a765e7778c65a16c8f14d663fcd0
Shanu001x <sraw419@gmail.com>
2024-08-03 15:47:14 +0530

    add new routes for User controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,32 +1,53 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logOutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUsercoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-
 const router = Router();
-    
+
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),    
-    registerUser
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-)
+router.route("/login").post(loginUser);
 
-router.route("/login").post(loginUser)
-
-router.route("/logout").post( verifyJWT,logOutUser)
+router.route("/logout").post(verifyJWT, logOutUser);
 
 // secured Routes
 
- router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUsercoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("watch-history").get(verifyJWT, getWatchHistory);
 
-export default router
+export default router;
